b0b8a86346beccaeba15fd860a04c9ce10ee826a
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2022-07-10 22:55:18 +0300

    added updateAvatar

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+const { createError } = require("../../helpers");
+const { User } = require("../../models/user");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { path: tempDir, originalname } = req.file;
+  const { _id } = req.user;
+  const [extension] = originalname.split(".").reverse();
+  const newName = `${_id}.${extension}`;
+
+  const file = await Jimp.read(tempDir);
+  const fileResize = file.resize(250, 250);
+  fileResize.write(tempDir);
+
+  const resultDir = path.join(avatarsDir, newName);
+
+  await fs.rename(tempDir, resultDir);
+
+  const avatarURL = path.join("avatars", newName);
+  const user = await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
+  if (!user.avatarURL) {
+    throw createError(401);
+  }
+  console.log(user);
+
+  res.json({ avatarURL: user.avatarURL });
+};
+
+module.exports = updateAvatar;
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,5 +2,6 @@ const validation = require("./validation");
 const isValidId = require("./isValidId");
 const hasBody = require("./hasBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
-module.exports = { validation, isValidId, hasBody, authenticate };
+module.exports = { validation, isValidId, hasBody, authenticate, upload };
